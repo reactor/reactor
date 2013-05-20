465acef451631751b847d0c89e0705359edd629d
Jon Brisbin <jbrisbin@gopivotal.com>
2013-05-20 11:39:15 -0500

    Add test for await.

---
+++ reactor-core/src/test/java/reactor/core/AwaitTests.java
@@ -0,0 +1,48 @@
+package reactor.core;
+
+import org.junit.Test;
+import reactor.fn.Consumer;
+import reactor.fn.dispatch.ThreadPoolExecutorDispatcher;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * @author Jon Brisbin
+ */
+@SuppressWarnings({"unchecked", "rawtypes"})
+public class AwaitTests {
+
+	@Test
+	public void testAwaitDoesntBlockUnnecessarily() throws InterruptedException {
+		for (int i = 0; i < 1000; i++) {
+			Reactor reactor = new Reactor();
+			Reactor innerReactor = new Reactor(new ThreadPoolExecutorDispatcher(4, 64));
+
+			final Promise<String> promise = new Promise<String>(reactor);
+			final CountDownLatch latch = new CountDownLatch(1);
+
+			R.schedule(new Consumer() {
+
+				@Override
+				public void accept(Object t) {
+					promise.set("foo");
+
+				}
+
+			}, null, innerReactor);
+
+			promise.onSuccess(new Consumer<String>() {
+
+				@Override
+				public void accept(String t) {
+					latch.countDown();
+				}
+			});
+
+			assertThat("latch is counted down", latch.await(5, TimeUnit.SECONDS));
+		}
+	}
+}
