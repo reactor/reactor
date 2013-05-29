dc1833469cfe7e20c9cd35855c0c3aad84d982e3
Jon Brisbin <jon@jbrisbin.com>
2013-05-29 14:55:45 -0500

    Eliminate compiler warnings.

--- reactor-core/src/main/java/reactor/core/ComponentSpec.java
+++ reactor-core/src/main/java/reactor/core/ComponentSpec.java
@@ -26,7 +26,6 @@ public abstract class ComponentSpec<SPEC extends ComponentSpec<SPEC, TARGET>, TA
 	protected Registry.LoadBalancingStrategy loadBalancingStrategy;
 	protected SelectionStrategy              selectionStrategy;
 
-	@SuppressWarnings("unchecked")
 	public SPEC using(Environment env) {
 		this.env = env;
 		return (SPEC) this;
@@ -37,88 +36,74 @@ public abstract class ComponentSpec<SPEC extends ComponentSpec<SPEC, TARGET>, TA
 		return (SPEC) this;
 	}
 
-	@SuppressWarnings("unchecked")
 	public SPEC using(Converter converter) {
 		this.converter = converter;
 		return (SPEC) this;
 	}
 
-	@SuppressWarnings("unchecked")
 	public SPEC using(SelectionStrategy selectionStrategy) {
 		this.selectionStrategy = selectionStrategy;
 		return (SPEC) this;
 	}
 
-	@SuppressWarnings("unchecked")
 	public SPEC using(Converter... converters) {
 		this.converter = new DelegatingConverter(converters);
 		return (SPEC) this;
 	}
 
-	@SuppressWarnings("unchecked")
 	public SPEC using(List<Converter> converters) {
 		this.converter = new DelegatingConverter(converters);
 		return (SPEC) this;
 	}
 
-	@SuppressWarnings("unchecked")
 	public SPEC using(Registry.LoadBalancingStrategy loadBalancingStrategy) {
 		this.loadBalancingStrategy = loadBalancingStrategy;
 		return (SPEC) this;
 	}
 
-	@SuppressWarnings("unchecked")
 	public SPEC broadcastLoadBalancing() {
 		this.loadBalancingStrategy = Registry.LoadBalancingStrategy.NONE;
 		return (SPEC) this;
 	}
 
-	@SuppressWarnings("unchecked")
 	public SPEC randomLoadBalancing() {
 		this.loadBalancingStrategy = Registry.LoadBalancingStrategy.RANDOM;
 		return (SPEC) this;
 	}
 
-	@SuppressWarnings("unchecked")
 	public SPEC roundRobinLoadBalancing() {
 		this.loadBalancingStrategy = Registry.LoadBalancingStrategy.ROUND_ROBIN;
 		return (SPEC) this;
 	}
 
-	@SuppressWarnings("unchecked")
 	public SPEC tagFiltering() {
 		this.selectionStrategy = new TagAwareSelectionStrategy();
 		return (SPEC) this;
 	}
 
-	@SuppressWarnings("unchecked")
 	public SPEC sync() {
 		this.dispatcher = SynchronousDispatcher.INSTANCE;
 		return (SPEC) this;
 	}
 
-	@SuppressWarnings("unchecked")
 	public SPEC threadPoolExecutor() {
 		Assert.notNull(env, "Cannot use a thread pool Dispatcher without a properly-configured Environment.");
 		this.dispatcher = env.getDispatcher(Environment.THREAD_POOL_EXECUTOR_DISPATCHER);
 		return (SPEC) this;
 	}
 
-	@SuppressWarnings("unchecked")
 	public SPEC eventLoop() {
 		Assert.notNull(env, "Cannot use an event loop Dispatcher without a properly-configured Environment.");
 		this.dispatcher = env.getDispatcher(Environment.EVENT_LOOP_DISPATCHER);
 		return (SPEC) this;
 	}
 
-	@SuppressWarnings("unchecked")
 	public SPEC ringBuffer() {
 		Assert.notNull(env, "Cannot use an RingBuffer Dispatcher without a properly-configured Environment.");
 		this.dispatcher = env.getDispatcher(Environment.RING_BUFFER_DISPATCHER);
 		return (SPEC) this;
 	}
 
-	@SuppressWarnings("unchecked")
 	public SPEC dispatcher(Dispatcher dispatcher) {
 		this.dispatcher = dispatcher;
 		return (SPEC) this;
