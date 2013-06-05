d4f615b4289714c70f44cdf0f8cf96d6c2c4400b
Jon Brisbin <jbrisbin@gopivotal.com>
2013-06-05 14:10:17 -0500

    Fixes for AsyncAppender, added speed tests.

--- reactor-logback/src/main/java/reactor/logback/AsyncAppender.java
+++ reactor-logback/src/main/java/reactor/logback/AsyncAppender.java
@@ -15,18 +15,28 @@ import com.lmax.disruptor.dsl.ProducerType;
 import java.util.Iterator;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author Jon Brisbin
  */
 public class AsyncAppender extends UnsynchronizedAppenderBase<ILoggingEvent> implements AppenderAttachable<ILoggingEvent> {
 
-	private final AppenderAttachableImpl<ILoggingEvent> attachable      = new AppenderAttachableImpl<ILoggingEvent>();
-	private       boolean                               alreadyAttached = false;
-	private final Disruptor<LogEvent>  disruptor;
-	private       RingBuffer<LogEvent> ringBuffer;
+	private final AppenderAttachableImpl<ILoggingEvent>    aai      = new AppenderAttachableImpl<ILoggingEvent>();
+	private final AtomicReference<Appender<ILoggingEvent>> delegate = new AtomicReference<Appender<ILoggingEvent>>();
+	private       int                                      backlog  = 8 * 1024;
+	private Disruptor<LogEvent>  disruptor;
+	private RingBuffer<LogEvent> ringBuffer;
 
 	public AsyncAppender() {
+	}
+
+	public void setBacklog(int backlog) {
+		this.backlog = backlog;
+	}
+
+	@Override
+	public void start() {
 		disruptor = new Disruptor<LogEvent>(
 				new EventFactory<LogEvent>() {
 					@Override
@@ -34,82 +44,85 @@ public class AsyncAppender extends UnsynchronizedAppenderBase<ILoggingEvent> imp
 						return new LogEvent();
 					}
 				},
-				512,
-				Executors.newFixedThreadPool(1, new ThreadFactory() {
-					@Override
-					public Thread newThread(Runnable r) {
-						Thread t = new Thread(r);
-						t.setName("logback-ringbuffer-appender");
-						t.setPriority(Thread.NORM_PRIORITY);
-						t.setDaemon(true);
-						return t;
-					}
-				}),
+				backlog,
+				Executors.newFixedThreadPool(
+						1,
+						new ThreadFactory() {
+							@Override
+							public Thread newThread(Runnable r) {
+								Thread t = new Thread(r);
+								t.setName("logback-ringbuffer-appender");
+								t.setPriority(Thread.MAX_PRIORITY);
+								t.setDaemon(true);
+								return t;
+							}
+						}
+				),
 				ProducerType.MULTI,
 				new BlockingWaitStrategy()
 		);
-
 		disruptor.handleEventsWith(new LoggingEventHandler());
-	}
-
-	@Override
-	public void start() {
 		ringBuffer = disruptor.start();
+
 		super.start();
 	}
 
 	@Override
 	public void stop() {
 		disruptor.shutdown();
+		if (null != delegate.get()) {
+			delegate.get().stop();
+		}
 		super.stop();
 	}
 
 	@Override
 	protected void append(ILoggingEvent loggingEvent) {
-		loggingEvent.prepareForDeferredProcessing();
-		long seq = ringBuffer.next();
-		ringBuffer.get(seq).event = loggingEvent;
-		ringBuffer.publish(seq);
+		if (null != delegate.get()) {
+			long seq = ringBuffer.next();
+			LogEvent evt = ringBuffer.get(seq);
+			evt.event = loggingEvent;
+			ringBuffer.publish(seq);
+		}
 	}
 
 	@Override
 	public void addAppender(Appender<ILoggingEvent> newAppender) {
-		if (!alreadyAttached) {
-			alreadyAttached = true;
-			attachable.addAppender(newAppender);
+		if (delegate.compareAndSet(null, newAppender)) {
+			aai.addAppender(newAppender);
 		} else {
-			addWarn("Cannot attach more than one Appender. Ignoring " + newAppender);
+			throw new IllegalArgumentException(delegate.get() + " already attached.");
 		}
 	}
 
 	@Override
 	public Iterator<Appender<ILoggingEvent>> iteratorForAppenders() {
-		return attachable.iteratorForAppenders();
+		return aai.iteratorForAppenders();
 	}
 
 	@Override
 	public Appender<ILoggingEvent> getAppender(String name) {
-		return attachable.getAppender(name);
+		return aai.getAppender(name);
 	}
 
 	@Override
 	public boolean isAttached(Appender<ILoggingEvent> appender) {
-		return attachable.isAttached(appender);
+		return aai.isAttached(appender);
 	}
 
 	@Override
 	public void detachAndStopAllAppenders() {
-		attachable.detachAndStopAllAppenders();
+		aai.detachAndStopAllAppenders();
 	}
 
 	@Override
 	public boolean detachAppender(Appender<ILoggingEvent> appender) {
-		return attachable.detachAppender(appender);
+		return aai.detachAppender(appender);
 	}
 
 	@Override
 	public boolean detachAppender(String name) {
-		return attachable.detachAppender(name);
+		return aai.detachAppender(name);
 	}
 
 	private static class LogEvent {
@@ -119,7 +132,8 @@ public class AsyncAppender extends UnsynchronizedAppenderBase<ILoggingEvent> imp
 	private class LoggingEventHandler implements EventHandler<LogEvent> {
 		@Override
 		public void onEvent(LogEvent logEvent, long l, boolean b) throws Exception {
-			attachable.appendLoopOnAppenders(logEvent.event);
+			aai.appendLoopOnAppenders(logEvent.event);
+			logEvent.event = null;
 		}
 	}
 
--- reactor-logback/src/test/java/reactor/logback/AsyncAppenderTests.java
+++ reactor-logback/src/test/java/reactor/logback/AsyncAppenderTests.java
@@ -1,10 +1,17 @@
 package reactor.logback;
 
 import ch.qos.logback.classic.Logger;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.LoggerFactory;
 
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
@@ -28,6 +35,19 @@ public class AsyncAppenderTests {
 		MSG = new String(chars);
 	}
 
+	final int timeout = 5;
+	ExecutorService threadPool;
+
+	@Before
+	public void setup() {
+		threadPool = Executors.newCachedThreadPool();
+	}
+
+	@After
+	public void cleanup() {
+		threadPool.shutdownNow();
+	}
+
 	@Test
 	public void asyncAppenderLogsAsynchronously() {
 		Logger log = (Logger) LoggerFactory.getLogger(AsyncAppenderTests.class);
@@ -35,6 +55,45 @@ public class AsyncAppenderTests {
 		log.info(MSG);
 
 		assertThat("Appender is not set", log.iteratorForAppenders().next(), instanceOf(AsyncAppender.class));
+	}
+
+	@Test
+	public void clockAsyncAppender() throws InterruptedException {
+		long n = benchmarkThread((Logger) LoggerFactory.getLogger("reactor"), timeout);
+		System.out.println("async: " + (n / timeout) + "/sec");
+	}
+
+	@Test
+	public void clockSyncAppender() throws InterruptedException {
+		long m = benchmarkThread((Logger) LoggerFactory.getLogger("sync"), timeout);
+		System.out.println("sync: " + (m / timeout) + "/sec");
+	}
+
+	@Test
+	public void clockBothAppenders() throws InterruptedException {
+		clockSyncAppender();
+		clockAsyncAppender();
+	}
+
+	private long benchmarkThread(final Logger logger, int timeout) throws InterruptedException {
+		final CountDownLatch latch = new CountDownLatch(1);
+		final AtomicLong throughput = new AtomicLong(0);
+
+		int threads = Runtime.getRuntime().availableProcessors();
+		for (int i = 0; i < threads; i++) {
+			threadPool.submit(new Runnable() {
+				@Override
+				public void run() {
+					while (latch.getCount() > 0) {
+						logger.warn("" + throughput.incrementAndGet());
+					}
+				}
+			});
+		}
+
+		latch.await(timeout, TimeUnit.SECONDS);
+		latch.countDown();
+		return throughput.get();
 	}
 
 }
