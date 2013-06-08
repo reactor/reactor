c8952ba5c14fd86dfbfe315d83e25d3124be216e
Jon Brisbin <jbrisbin@gopivotal.com>
2013-06-08 14:47:11 -0500

    Tweaks to FileChannelConsumer.

--- reactor-core/src/main/java/reactor/io/file/FileChannelConsumer.java
+++ reactor-core/src/main/java/reactor/io/file/FileChannelConsumer.java
@@ -11,14 +11,25 @@ import java.nio.channels.FileChannel;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
+ * A {@link Consumer} implementation that dumps everything it accepts to an open {@link FileChannel}.
+ *
  * @author Jon Brisbin
  */
 public class FileChannelConsumer<T> implements Consumer<T> {
 
+	private final File             outFile;
 	private final FileOutputStream out;
 	private final FileChannel      channel;
 	private final AtomicLong position = new AtomicLong(0);
 
+	/**
+	 * Create a new timestamped file in the given directory, using the given basename. A timestamp will be append to the
+	 * basename so no files will ever be overwritten using this {@link Consumer}.
+	 *
+	 * @param dir      The directory in which to create the file.
+	 * @param basename The basename to use, to which will be appended a timestamp.
+	 * @throws FileNotFoundException
+	 */
 	public FileChannelConsumer(String dir, String basename) throws FileNotFoundException {
 		File d = new File(dir);
 		if (!d.exists()) {
@@ -26,8 +37,18 @@ public class FileChannelConsumer<T> implements Consumer<T> {
 		}
 
 		String filename = basename + "-" + System.currentTimeMillis();
-		out = new FileOutputStream(new File(d, filename));
-		channel = out.getChannel();
+		this.outFile = new File(d, filename);
+		this.out = new FileOutputStream(outFile);
+		this.channel = out.getChannel();
+	}
+
+	/**
+	 * Get the {@link File} object for the currently-open {@link FileChannel}.
+	 *
+	 * @return The {@link File} for this channel.
+	 */
+	public File getFile() {
+		return outFile;
 	}
 
 	@Override
