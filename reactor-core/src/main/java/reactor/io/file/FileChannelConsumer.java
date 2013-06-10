449bb8354a9b677bc8f754754fa25994edfbceab
Jon Brisbin <jbrisbin@gopivotal.com>
2013-06-10 08:05:06 -0500

    Add a Converter and better object handling in FileChannelConsumer.

--- reactor-core/src/main/java/reactor/io/file/FileChannelConsumer.java
+++ reactor-core/src/main/java/reactor/io/file/FileChannelConsumer.java
@@ -1,5 +1,6 @@
 package reactor.io.file;
 
+import reactor.convert.Converter;
 import reactor.fn.Consumer;
 import reactor.io.Buffer;
 
@@ -21,6 +22,7 @@ public class FileChannelConsumer<T> implements Consumer<T> {
 	private final FileOutputStream out;
 	private final FileChannel      channel;
 	private final AtomicLong position = new AtomicLong(0);
+	private final Converter converter;
 
 	/**
 	 * Create a new timestamped file in the given directory, using the given basename. A timestamp will be append to the
@@ -31,6 +33,21 @@ public class FileChannelConsumer<T> implements Consumer<T> {
 	 * @throws FileNotFoundException
 	 */
 	public FileChannelConsumer(String dir, String basename) throws FileNotFoundException {
+		this(dir, basename, null);
+	}
+
+	/**
+	 * Create a new timestamped file in the given directory, using the given basename. A timestamp will be append to the
+	 * basename so no files will ever be overwritten using this {@link Consumer}.
+	 *
+	 * @param dir       The directory in which to create the file.
+	 * @param basename  The basename to use, to which will be appended a timestamp.
+	 * @param converter The {@link Converter} to use to turn accepted objects into {@link Buffer Buffers}.
+	 * @throws FileNotFoundException
+	 */
+	public FileChannelConsumer(String dir, String basename, Converter converter) throws FileNotFoundException {
+		this.converter = converter;
+
 		File d = new File(dir);
 		if (!d.exists()) {
 			d.mkdirs();
@@ -53,8 +70,18 @@ public class FileChannelConsumer<T> implements Consumer<T> {
 
 	@Override
 	public void accept(T t) {
+		if (null == t) {
+			return;
+		}
 		try {
-			Buffer b = Buffer.wrap(t.toString());
+			Buffer b;
+			if (Buffer.class.isInstance(t)) {
+				b = (Buffer) t;
+			} else if (null != converter && converter.canConvert(t.getClass(), Buffer.class)) {
+				b = converter.convert(t, Buffer.class);
+			} else {
+				b = Buffer.wrap(t.toString());
+			}
 			channel.write(b.byteBuffer(), position.getAndAdd(b.remaining()));
 		} catch (IOException e) {
 			throw new IllegalStateException(e);
