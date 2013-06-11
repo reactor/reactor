a95afd2b1f9518ef063e922ac9954fc1d826300b
Stephane Maldini <smaldini@gopivotal.com>
2013-06-11 12:30:09 +0100

    update copyrights

--- reactor-core/src/test/java/reactor/fn/SelectorUnitTests.java
+++ reactor-core/src/test/java/reactor/fn/SelectorUnitTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2013 the original author or authors.
+ * Copyright (c) 2011-2013 GoPivotal, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
