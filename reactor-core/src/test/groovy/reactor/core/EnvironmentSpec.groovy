6ad389242191a6983ca85e77755769a8110ad3fb
Andy Wilkinson <awilkinson@gopivotal.com>
2013-06-21 13:19:33 +0100

    Use a mock rather than thread count
    Using live thread count isn't reliable as the number of live threads
    may change for reasons outside the control of the test. Instead,
    use mock Dispatchers and verify that their shutdown method has been
    called.

--- reactor-core/src/test/groovy/reactor/core/EnvironmentSpec.groovy
+++ reactor-core/src/test/groovy/reactor/core/EnvironmentSpec.groovy
@@ -3,21 +3,27 @@ package reactor.core
 import java.lang.management.ManagementFactory
 import java.lang.management.ThreadMXBean
 
+import reactor.core.configuration.ConfigurationReader;
+import reactor.core.configuration.ReactorConfiguration
+import reactor.fn.dispatch.Dispatcher
 import spock.lang.Specification
 
 class EnvironmentSpec extends Specification {
 
 	def "An environment cleans up its Dispatchers when it's shut down"() {
-		given: "An Environment"
-		ThreadMXBean threadMxBean = ManagementFactory.getThreadMXBean()
-		int threadCount = threadMxBean.threadCount
 
-		Environment environment = new Environment()
+		given: "An Environment"
+
+		ReactorConfiguration configuration = new ReactorConfiguration([], 'default', [:] as Properties)
+		Dispatcher dispatcher = Mock(Dispatcher)
+		Environment environment = new Environment(['alpha':[dispatcher, dispatcher], 'bravo':[dispatcher]], Mock(ConfigurationReader, {
+			read() >> configuration
+		}))
 
 		when: "it is shut down"
 		environment.shutdown()
 
 		then: "its dispatchers are cleaned up"
-		threadCount == threadMxBean.threadCount
+		3 * dispatcher.shutdown()
 	}
 }
