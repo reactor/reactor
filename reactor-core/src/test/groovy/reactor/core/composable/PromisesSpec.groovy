48905ff4678919d4eeaadffaa14d9c1a27a8ab58
Stephane Maldini <smaldini@gopivotal.com>
2013-07-09 07:14:17 +0100

    update copyrights

--- reactor-core/src/test/groovy/reactor/core/composable/PromisesSpec.groovy
+++ reactor-core/src/test/groovy/reactor/core/composable/PromisesSpec.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2013 GoPivotal, Inc. All Rights Reservedeferred.
+ * Copyright (c) 2011-2013 GoPivotal, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or impliedeferred.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
