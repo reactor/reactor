c48ee23906756fbdceb047669ad069eccbb75cc3
Stephane Maldini <smaldini@gopivotal.com>
2013-07-19 09:53:01 +0100

    update nettyTcp

--- reactor-tcp/src/main/java/reactor/tcp/netty/NettyTcpConnectionChannelInboundHandler.java
+++ reactor-tcp/src/main/java/reactor/tcp/netty/NettyTcpConnectionChannelInboundHandler.java
@@ -19,7 +19,6 @@ package reactor.tcp.netty;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.MessageList;
 import reactor.io.Buffer;
 
 class NettyTcpConnectionChannelInboundHandler extends ChannelInboundHandlerAdapter {
@@ -32,43 +31,40 @@ class NettyTcpConnectionChannelInboundHandler extends ChannelInboundHandlerAdapt
 		this.conn = conn;
 	}
 
+
 	@Override
-	public final void messageReceived(ChannelHandlerContext ctx, MessageList<Object> msgs) throws Exception {
-		int size = msgs.size();
-		for (int i = 0; i < size; i ++) {
-			Object m = msgs.get(i);
-			if (m instanceof ByteBuf) {
-				ByteBuf data = (ByteBuf) m;
+	public void channelRead(ChannelHandlerContext ctx, Object m) throws Exception {
+		if (m instanceof ByteBuf) {
+			ByteBuf data = (ByteBuf) m;
 
-				if (remainder == null) {
-					try {
-						passToConnection(data);
-					} finally {
-						if (data.isReadable()) {
-							remainder = data;
-						} else {
-							data.release();
-						}
-					}
-				} else {
-					if (!bufferHasSufficientCapacity(remainder, data)) {
-						ByteBuf combined = createCombinedBuffer(remainder, data, ctx);
-						remainder.release();
-						remainder = combined;
+			if (remainder == null) {
+				try {
+					passToConnection(data);
+				} finally {
+					if (data.isReadable()) {
+						remainder = data;
 					} else {
-						remainder.writeBytes(data);
+						data.release();
 					}
-					data.release();
+				}
+			} else {
+				if (!bufferHasSufficientCapacity(remainder, data)) {
+					ByteBuf combined = createCombinedBuffer(remainder, data, ctx);
+					remainder.release();
+					remainder = combined;
+				} else {
+					remainder.writeBytes(data);
+				}
+				data.release();
 
-					try {
-						passToConnection(remainder);
-					} finally {
-						if (remainder.isReadable()) {
-							remainder.discardSomeReadBytes();
-						} else {
-							remainder.release();
-							remainder = null;
-						}
+				try {
+					passToConnection(remainder);
+				} finally {
+					if (remainder.isReadable()) {
+						remainder.discardSomeReadBytes();
+					} else {
+						remainder.release();
+						remainder = null;
 					}
 				}
 			}
