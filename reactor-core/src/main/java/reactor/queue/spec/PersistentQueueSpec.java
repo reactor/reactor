dc190a3f4d2bfa2bf6cc36a859c1b9cf09ca43a3
Jon Brisbin <jbrisbin@gopivotal.com>
2013-07-31 11:12:07 -0500

    Add indexedChronicle() simple configuration option on PersistenQueueSpec.

--- reactor-core/src/main/java/reactor/queue/spec/PersistentQueueSpec.java
+++ reactor-core/src/main/java/reactor/queue/spec/PersistentQueueSpec.java
@@ -1,11 +1,16 @@
 package reactor.queue.spec;
 
+import java.io.IOException;
+
 import reactor.function.Supplier;
 import reactor.queue.InMemoryQueuePersistor;
+import reactor.queue.IndexedChronicleQueuePersistor;
 import reactor.queue.PersistentQueue;
 import reactor.util.Assert;
 
 /**
+ * Helper spec to create a {@link PersistentQueue} instance.
+ *
  * @author Jon Brisbin
  */
 public class PersistentQueueSpec<T> implements Supplier<PersistentQueue<T>> {
@@ -15,6 +20,16 @@ public class PersistentQueueSpec<T> implements Supplier<PersistentQueue<T>> {
 	public PersistentQueueSpec<T> inMemory() {
 		Assert.isNull(queue, "PersistentQueue type already set (" + queue.getClass().getName() + ")");
 		this.queue = new PersistentQueue<T>(new InMemoryQueuePersistor<T>());
+		return this;
+	}
+
+	public PersistentQueueSpec<T> indexedChronicle(String basePath) {
+		Assert.isNull(queue, "PersistentQueue type already set (" + queue.getClass().getName() + ")");
+		try {
+			this.queue = new PersistentQueue<T>(new IndexedChronicleQueuePersistor<T>(basePath));
+		} catch(IOException e) {
+			throw new IllegalArgumentException(e.getMessage(), e);
+		}
 		return this;
 	}
 
