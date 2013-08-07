541d675b4c6df22151a48719d31b637f722fc22f
Michael Nitschinger <michael@nitschinger.at>
2013-08-07 14:50:27 +0200

    Adding javadoc for the processor.Operation class.

--- reactor-core/src/main/java/reactor/core/processor/Operation.java
+++ reactor-core/src/main/java/reactor/core/processor/Operation.java
@@ -3,6 +3,16 @@ package reactor.core.processor;
 import reactor.function.Supplier;
 
 /**
+ * A {@link Operation} represents the payload that gets (re) used in the processor.
+ *
+ * The {@link Operation} itself gets allocated during creation time for the {@link Processor}, so at
+ * runtime the instance will be reused. What changes during runtime is the enclosed payload.
+ * </p>
+ * In addition to the payload, the {@link #id} represents the corresponding ID in the underlying
+ * data structure (in the general case a {@link com.lmax.disruptor.RingBuffer}.
+ *
+ * @param <T> the type of the supplied object
+ *
  * @author Jon Brisbin
  */
 public abstract class Operation<T> implements Supplier<T> {
@@ -10,18 +20,36 @@ public abstract class Operation<T> implements Supplier<T> {
 	protected volatile Long id;
 	private final      T    data;
 
+  /**
+   * Create a new {@link Operation} and apply the given payload.
+   *
+   * @param data the payload to store.
+   */
 	Operation(T data) {
 		this.data = data;
 	}
 
+  /**
+   * Set the identifier for the underlying payload.
+   *
+   * @param id the identifier, most likely a sequnce number.
+   */
 	void setId(Long id) {
 		this.id = id;
 	}
 
+  /**
+   * Get the {@link Operation} payload.
+   *
+   * @return the enclosed payload.
+   */
 	@Override public T get() {
 		return data;
 	}
 
+  /**
+   * Commit the {@link Operation} to the underlying datastructure.
+   */
 	public abstract void commit();
 
 }
