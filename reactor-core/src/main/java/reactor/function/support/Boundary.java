a5f19f70769551aa2a28a88c433c7846cdddf3e5
Jon Brisbin <jbrisbin@gopivotal.com>
2013-09-04 10:25:16 -0500

    Create a Boundary abstraction to which users can bind arbitrary Consumers. Calling the `bind(Consumer)` method returns a new Consumer that manages the internal state of calling the internal CountDownLatch after calling the delegate Consumer. This means you can bind an entire flow of Consumers to a Boundary and then block the calling thread until all bound Consumers have been called.
    The `await(long, TimeUnit)` method will divide up the timeout among all the bound Consumers. e.g. if the total timeout specified is 5 seconds and the first Consumer takes 3 seconds to complete and the second one takes 2, the third Consumer will not have any time left in which to await, so the await method will timeout and return false.
    
    No Consumers are cancelled by a Boundary. It is completely passive. It can be called multiple times, as well. If a Consumer has already been invoked, it will simply be skipped.

---
+++ reactor-core/src/main/java/reactor/function/support/Boundary.java
@@ -0,0 +1,96 @@
+package reactor.function.support;
+
+import reactor.function.Consumer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * A {@code Boundary} is a blocking utility that allows the user to bind an arbitrary number of {@link Consumer
+ * Consumers} to it. Whenever {@link #bind(reactor.function.Consumer)} is called, it returns a new {@code Consumer} that
+ * internally creates a {@link CountDownLatch} which calls the delegate {@code Consumer}, then counts down the latch.
+ * Calling {@link #await()} or {@link #await(long, java.util.concurrent.TimeUnit)} on the {@code Boundary} will block
+ * the calling thread until all bound latches are released.
+ * <p/>
+ * The timeout value given is the total timeout value and not the per-latch timeout value. If a timeout of 5 seconds is
+ * specified and there are 100 latches bound, then all 100 latches have a combined 5 seconds to be counted down, not 5
+ * seconds per latch, which would equate to a little under 10 minutes.
+ *
+ * @author Jon Brisbin
+ */
+public class Boundary {
+
+	private final List<CountDownLatch> latches = new ArrayList<CountDownLatch>();
+
+	public Boundary() {
+	}
+
+	/**
+	 * Bind the given {@link Consumer} to this {@code Boundary} by creating a {@link CountDownLatch} that will be counted
+	 * down the first time the given {@link Consumer} is invoked.
+	 *
+	 * @param consumer The delegate {@code Consumer}
+	 * @param <T>      The type of the value accepted by the {@code Consumer}
+	 * @return A new {@code Consumer} which will count down the internal latch after invoking the delegate {@code
+	 *         Consumer}
+	 */
+	public <T> Consumer<T> bind(final Consumer<T> consumer) {
+		synchronized (latches) {
+			final CountDownLatch latch = new CountDownLatch(1);
+			latches.add(latch);
+
+			return new Consumer<T>() {
+				@Override
+				public void accept(T t) {
+					consumer.accept(t);
+					latch.countDown();
+				}
+			};
+		}
+	}
+
+	/**
+	 * Wait for all latches to be counted down (almost) indefinitely.
+	 *
+	 * @return {@code true} if all latches were counted down within the timeout value, {@code false} otherwise
+	 */
+	public boolean await() {
+		return await(Integer.MAX_VALUE, TimeUnit.SECONDS);
+	}
+
+	/**
+	 * Wait for all latches to be counted down within the given timeout window.
+	 *
+	 * @param timeout  The timeout value.
+	 * @param timeUnit The unit of time measured by the timeout value.
+	 * @return {@code true} if all latches were counted down within the timeout value, {@code false} otherwise
+	 */
+	public boolean await(long timeout, TimeUnit timeUnit) {
+		if (latches.isEmpty()) {
+			// we're not watching any latches
+			return true;
+		}
+
+		final long start = System.currentTimeMillis();
+		final long timeoutMillis = TimeUnit.MILLISECONDS.convert(timeout, timeUnit);
+		synchronized (latches) {
+			try {
+				long elapsed = 0;
+				for (CountDownLatch latch : latches) {
+					boolean b = latch.await(timeoutMillis - elapsed, TimeUnit.MILLISECONDS);
+					elapsed = System.currentTimeMillis() - start;
+					if (!b || elapsed >= timeoutMillis) {
+						return false;
+					}
+				}
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+
+		return (System.currentTimeMillis() - start < timeoutMillis);
+	}
+
+}
