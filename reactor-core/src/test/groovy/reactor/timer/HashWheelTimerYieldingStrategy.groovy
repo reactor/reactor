53c0dcfab40b91838694843729c85c2effe7272b
Alex Petrov <alexp@coffeenco.de>
2014-02-05 22:25:27 +0100

    This commit adds a Timer interface implemented by HashWheelTimer
    HashWheelTimer itself is implemented as per the "Hashed and Hierarchical
    Timing Wheels" paper: http://www.cs.columbia.edu/~nahum/w6998/papers/sosp87-timing-wheels.pdf
    
    This implementation doesn't involve hierarchical Timing Wheels.
    
    This implementation provides three types of wait strategies:
    
       * Sleep: least precise, but most scheduler-friendly, uses
         `Thread.sleep` under the hood.
       * Yielding: more precise, and scheduler firendly, uses `Thread.yield`
         under the hood.
       * BusySpin: most precise, least scheduler friendly, doesn't release
         flow control, therefore occupies an entire core on multi-core
         systems.
    
    Executors are configurable through the constructor. By default, single
    threaded executor is used.
    
    Loop thread name is configurable through the constructor.

---
+++ reactor-core/src/test/groovy/reactor/timer/HashWheelTimerYieldingStrategy.groovy
@@ -0,0 +1,72 @@
+package reactor.timer;
+
+import reactor.function.Consumer
+import spock.lang.Specification
+
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+/**
+ * @author Oleksandr Petrov
+ */
+class HashWheelTimerYieldingStrategy extends Specification {
+
+  def period = 50
+
+  def "HashWheelTimer can schedule recurring tasks"() {
+
+    given:
+    "a new timer"
+    def timer = new HashWheelTimer(10, 8, new HashWheelTimer.YieldingWait())
+    def latch = new CountDownLatch(10)
+
+    when:
+    "a task is submitted"
+    timer.schedule(
+            { Long now -> latch.countDown() } as Consumer<Long>,
+            period,
+            TimeUnit.MILLISECONDS,
+            period
+    )
+
+    then:
+    "the latch was counted down"
+    latch.await(1, TimeUnit.SECONDS)
+    timer.cancel()
+
+  }
+
+  def "HashWheelTimer can delay submitted tasks"() {
+
+    given:
+    "a new timer"
+    def delay = 500
+    def timer = new HashWheelTimer(10, 512, new HashWheelTimer.YieldingWait())
+    def latch = new CountDownLatch(1)
+    def start = System.currentTimeMillis()
+    def elapsed = 0
+    //def actualTimeWithinBounds = true
+
+    when:
+    "a task is submitted"
+    timer.submit(
+            { Long now ->
+              elapsed = System.currentTimeMillis() - start
+              latch.countDown()
+            } as Consumer<Long>,
+            delay,
+            TimeUnit.MILLISECONDS
+    )
+
+    then:
+    "the latch was counted down"
+    latch.await(1, TimeUnit.SECONDS)
+    elapsed >= delay
+    elapsed < delay * 2
+    timer.cancel()
+  }
+
+}
+
+
+
