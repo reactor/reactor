21db2d9bc20a1760b2833dfab520767944195495
Jon Brisbin <jon@jbrisbin.com>
2014-05-15 10:26:03 -0500

    Tweak ForkJoinPool to handle joining tasks into a Promise differently than forking tasks using a Stream

--- reactor-core/src/main/java/reactor/core/fork/ForkJoinPool.java
+++ reactor-core/src/main/java/reactor/core/fork/ForkJoinPool.java
@@ -1,5 +1,9 @@
 package reactor.core.fork;
 
+import com.gs.collections.api.list.ImmutableList;
+import com.gs.collections.impl.list.mutable.FastList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.Environment;
 import reactor.core.composable.Deferred;
 import reactor.core.composable.Promise;
@@ -9,9 +13,10 @@ import reactor.core.composable.spec.Streams;
 import reactor.event.dispatch.Dispatcher;
 import reactor.function.Function;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Simple {@link java.util.concurrent.Executor} backed fork/join pool that will coalesce results from asynchronous tasks
@@ -21,6 +26,8 @@ import java.util.concurrent.Executor;
  * @author Jon Brisbin
  */
 public class ForkJoinPool {
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
 	private final Environment env;
 	private final Dispatcher  dispatcher;
@@ -50,23 +57,53 @@ public class ForkJoinPool {
 	 *
 	 * @return fork/join task
 	 */
-	public <V> ForkJoinTask<List<V>, Promise<List<V>>> join(final Function<?, V>... tasks) {
-		Deferred<List<V>, Promise<List<V>>> d = Promises.defer(env, dispatcher);
-		ForkJoinTask<List<V>, Promise<List<V>>> t = new ForkJoinTask<List<V>, Promise<List<V>>>(executor, d);
-		t.add(new Function<Object, List<V>>() {
-			@SuppressWarnings("unchecked")
-			@Override
-			public List<V> apply(Object o) {
-				List<V> results = new ArrayList<V>();
-				for (Function task : tasks) {
-					V result = (V) task.apply(o);
-					if (null != result) {
-						results.add(result);
+	public <V> ForkJoinTask<ImmutableList<V>, Promise<ImmutableList<V>>> join(final Function<?, V>... tasks) {
+		return join(Arrays.asList(tasks));
+	}
+
+	/**
+	 * Asynchronously submit the given tasks, one submit per task, to the configured {@link java.util.concurrent.Executor}
+	 * and collecting the results in a {@link java.util.List} that will be the fulfillment of the {@link
+	 * reactor.core.composable.Promise} returned from {@link ForkJoinTask#compose()}.
+	 *
+	 * @param tasks
+	 * 		asynchronous tasks to execute
+	 * @param <V>
+	 * 		type of task result
+	 *
+	 * @return fork/join task
+	 */
+	public <V> ForkJoinTask<ImmutableList<V>, Promise<ImmutableList<V>>> join(final Collection<Function<?, V>> tasks) {
+		final Deferred<ImmutableList<V>, Promise<ImmutableList<V>>> d
+				= Promises.defer(env, dispatcher);
+		final ForkJoinTask<ImmutableList<V>, Promise<ImmutableList<V>>> t
+				= new ForkJoinTask<ImmutableList<V>, Promise<ImmutableList<V>>>(executor, d);
+
+		final AtomicInteger count = new AtomicInteger(tasks.size());
+		final FastList<V> results = FastList.newList();
+
+		for (final Function fn : tasks) {
+			t.add(new Function<Object, ImmutableList<V>>() {
+				@SuppressWarnings("unchecked")
+				@Override
+				public ImmutableList<V> apply(Object o) {
+					try {
+						V result = (V) fn.apply(o);
+						if (null != result) {
+							synchronized (results) {
+								results.add(result);
+							}
+						}
+					} finally {
+						if (count.decrementAndGet() == 0) {
+							d.accept(results.toImmutable());
+						}
 					}
+					return null;
 				}
-				return results;
-			}
-		});
+			});
+		}
+
 		return t;
 	}
 
