bef04c1210e0df858ed3d1d61286091e037d1a7c
Stephane Maldini <smaldini@gopivotal.com>
2014-10-09 12:06:27 +0100

    avoid double error on timeout

--- reactor-core/src/main/java/reactor/rx/action/TimeoutAction.java
+++ reactor-core/src/main/java/reactor/rx/action/TimeoutAction.java
@@ -43,7 +43,8 @@ public class TimeoutAction<T> extends Action<T, T> {
 	private final Consumer<Void> timeoutRequest = new Consumer<Void>() {
 		@Override
 		public void accept(Void aVoid) {
-			doError(new TimeoutException("No data signaled for " + timeout + "ms"));
+			if(finalState != null)
+				doError(new TimeoutException("No data signaled for " + timeout + "ms"));
 		}
 	};
 
