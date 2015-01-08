ddf88aea71f7a418aebe5f26da1d9b8fcde96fd3
Stephane Maldini <smaldini@gopivotal.com>
2015-01-08 18:15:42 +0000

    Try fixing issue with TCK

--- reactor-core/src/test/java/reactor/reactivestreams/tck/StreamIdentityProcessorTests.java
+++ reactor-core/src/test/java/reactor/reactivestreams/tck/StreamIdentityProcessorTests.java
@@ -117,7 +117,6 @@ public class StreamIdentityProcessorTests extends org.reactivestreams.tck.Identi
 			return Streams
 					.from(list)
 					.log("iterable-publisher")
-					.dispatchOn(env)
 					.filter(integer -> true)
 					.map(integer -> integer);
 
