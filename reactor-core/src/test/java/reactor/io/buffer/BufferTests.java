680bf5b5c1ff97e9dfc9e9a8253cdeea0e8e3230
sshcherbakov <sshcherbakov@gopivotal.com>
2015-02-27 09:35:27 +0100

    #438 improve unit test to leave static state in tact

--- reactor-core/src/test/java/reactor/io/buffer/BufferTests.java
+++ reactor-core/src/test/java/reactor/io/buffer/BufferTests.java
@@ -11,11 +11,20 @@ public class BufferTests {
 
 	@Test
 	public void testAutoExpand() {
-		Buffer b = new Buffer();
-		Buffer.SMALL_BUFFER_SIZE = 20;		// to speed up the test
-		Buffer.MAX_BUFFER_SIZE = 100;		
-		for(int i=0; i< Buffer.MAX_BUFFER_SIZE - Buffer.SMALL_BUFFER_SIZE; i++) {
-			b.append((byte)0x1);
+		
+		int initial_small_size = Buffer.SMALL_BUFFER_SIZE;
+		int initial_max_size = Buffer.MAX_BUFFER_SIZE;
+		try {
+			Buffer b = new Buffer();
+			Buffer.SMALL_BUFFER_SIZE = 20;		// to speed up the test
+			Buffer.MAX_BUFFER_SIZE = 100;		
+			for(int i=0; i< Buffer.MAX_BUFFER_SIZE - Buffer.SMALL_BUFFER_SIZE; i++) {
+				b.append((byte)0x1);
+			}
+		}
+		finally {
+			Buffer.SMALL_BUFFER_SIZE = initial_small_size;
+			Buffer.MAX_BUFFER_SIZE = initial_max_size;		
 		}
 	}
 
