26735f1d4a4d3ef77b1b8dabefa6bed8771ebd71
Stephane Maldini <smaldini@gopivotal.com>
2015-05-09 19:50:46 +0100

    New quick example

--- reactor-stream/src/test/java/reactor/rx/PopularTagTests.java
+++ reactor-stream/src/test/java/reactor/rx/PopularTagTests.java
@@ -68,7 +68,7 @@ public class PopularTagTests extends AbstractReactorTest {
 						.map(w -> Tuple.of(w, 1))
 						.window(1, SECONDS)
 						.flatMap(s ->
-										BiStreams.reduceByKey(s, persistentMap, (acc, next) -> acc + next)
+										BiStreams.reduceByKey(s, (acc, next) -> acc + next)
 												.sort((a, b) -> -a.t2.compareTo(b.t2))
 												.take(10)
 												.finallyDo(_s -> LOG.info("------------------------ window complete! ----------------------"))
