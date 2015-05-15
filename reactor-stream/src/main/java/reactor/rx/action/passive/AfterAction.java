3c01718588d9f25c33f1210c6b840acd6ca1c505
Stephane Maldini <smaldini@gopivotal.com>
2015-05-15 16:24:27 +0100

    internal adjusting More streams doc

--- reactor-stream/src/main/java/reactor/rx/action/passive/AfterAction.java
+++ reactor-stream/src/main/java/reactor/rx/action/passive/AfterAction.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package reactor.rx.action.control;
+package reactor.rx.action.passive;
 
 import reactor.rx.action.Action;
 
