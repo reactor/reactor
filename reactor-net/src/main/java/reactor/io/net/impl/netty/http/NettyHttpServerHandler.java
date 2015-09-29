be56b1621cad7125ea9eb3efd311c2e05e45e8c3
Stephane Maldini <smaldini@gopivotal.com>
2015-09-29 18:00:04 +0100

    fix #510

--- reactor-net/src/main/java/reactor/io/net/impl/netty/http/NettyHttpServerHandler.java
+++ reactor-net/src/main/java/reactor/io/net/impl/netty/http/NettyHttpServerHandler.java
@@ -77,7 +77,7 @@ public class NettyHttpServerHandler<IN, OUT> extends NettyChannelHandlerBridge<I
 				@Override
 				public void onError(Throwable t) {
 					log.error("Error processing connection. Closing the channel.", t);
-					if (channelSubscription == null && CHANNEL_REF.get(NettyHttpServerHandler.this) == 0) {
+					if (CHANNEL_REF.get(NettyHttpServerHandler.this) == 0) {
 						ctx.channel().close();
 					}
 				}
