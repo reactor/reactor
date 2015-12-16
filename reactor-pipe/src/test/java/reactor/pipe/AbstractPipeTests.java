dcd9a674531bccd936adc8e4acb92aee4234361e
Alex Petrov <oleksandr.petrov@gmail.com>
2015-12-16 16:05:02 +0100

    Fix tests

--- reactor-pipe/src/test/java/reactor/pipe/AbstractPipeTests.java
+++ reactor-pipe/src/test/java/reactor/pipe/AbstractPipeTests.java
@@ -1,6 +1,15 @@
 package reactor.pipe;
 
+import org.junit.Test;
+import reactor.core.processor.RingBufferWorkProcessor;
+import reactor.pipe.key.Key;
+import reactor.pipe.registry.ConcurrentRegistry;
+import reactor.pipe.router.NoOpRouter;
+
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -8,55 +17,53 @@ import static org.hamcrest.core.Is.is;
 @SuppressWarnings("unchecked")
 public abstract class AbstractPipeTests extends AbstractRawBusTests {
 
-  protected abstract <T, O> void subscribe(IPipe.IPipeEnd<T, O> pipe);
+    protected abstract <T, O> void subscribe(IPipe.IPipeEnd<T, O> pipe);
 
-  protected abstract <T, O> void subscribeAndDispatch(IPipe.IPipeEnd<T, O> pipe, List<T> value);
+    protected abstract <T, O> void subscribeAndDispatch(IPipe.IPipeEnd<T, O> pipe, List<T> value);
 
-  protected abstract <T, O> void subscribeAndDispatch(IPipe.IPipeEnd<T, O> pipe, T value);
+    protected abstract <T, O> void subscribeAndDispatch(IPipe.IPipeEnd<T, O> pipe, T value);
 
 
+    @Test
+    public void testSmoke() throws InterruptedException { // Tests backpressure and in-thread dispatches
+        RingBufferWorkProcessor<Runnable> processor = RingBufferWorkProcessor.<Runnable>create(
+            Executors.newFixedThreadPool(4),
+            256);
+        RawBus<Key, Object> bus = new RawBus<Key, Object>(new ConcurrentRegistry<>(),
+                                                               processor,
+                                                               4,
+                                                               new NoOpRouter<>(),
+                                                               null,
+                                                               null);
 
-//  @Test
-//  public void testSmoke() throws InterruptedException { // Tests backpressure and in-thread dispatches
-//    Firehose<Key> concurrentFirehose = new Firehose<>(new ConcurrentRegistry<>(),
-//                                                      RingBufferWorkProcessor.create(Executors.newFixedThreadPool(4),
-//                                                                                     256),
-//                                                      4,
-//                                                      new Consumer<Throwable>() {
-//                                                        @Override
-//                                                        public void accept(Throwable throwable) {
-//                                                          System.out.printf("Exception caught while dispatching: %s\n",
-//                                                                            throwable.getMessage());
-//                                                          throwable.printStackTrace();
-//                                                        }
-//                                                      });
-//
-//    int iterations = 2000;
-//    CountDownLatch latch = new CountDownLatch(iterations);
-//
-//    subscribe(
-//      Pipe.<Integer>build()
-//        .map((i) -> i + 1)
-//        .map(i -> {
-//          try {
-//            Thread.sleep(10);
-//          } catch (InterruptedException e) {
-//            e.printStackTrace();
-//          }
-//          return i * 2;
-//        })
-//        .consume((i_) -> latch.countDown()));
-//
-//    for (int i = 0; i < iterations; i++) {
-//      firehose.notify(Key.wrap("source", "first"), i);
-//      if (i % 500 == 0) {
-//        System.out.println("Processed " + i + " keys");
-//      }
-//    }
-//
-//    latch.await(5, TimeUnit.MINUTES);
-//    assertThat(latch.getCount(), is(0L));
-//
-//    concurrentFirehose.shutdown();
-//  }
+
+        int iterations = 2000;
+        CountDownLatch latch = new CountDownLatch(iterations);
+
+        Pipe.<Integer>build()
+            .map((i) -> i + 1)
+            .map(i -> {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return i * 2;
+            })
+            .consume((i_) -> {
+                latch.countDown();
+            }).subscribe(Key.wrap("source", "first"), bus);
+
+        for (int i = 0; i < iterations; i++) {
+            bus.notify(Key.wrap("source", "first"), i);
+            if (i % 500 == 0) {
+                System.out.println("Processed " + i + " keys");
+            }
+        }
+
+        latch.await(5, TimeUnit.MINUTES);
+        assertThat(latch.getCount(), is(0L));
+
+        processor.shutdown();
+    }
 }
--- reactor-pipe/src/test/java/reactor/pipe/MatchedPipeTests.java
+++ reactor-pipe/src/test/java/reactor/pipe/MatchedPipeTests.java
@@ -14,6 +14,7 @@ import static org.hamcrest.core.Is.is;
 
 public class MatchedPipeTests extends AbstractPipeTests {
 
+  @SuppressWarnings("unchecked")
   @Test
   public void testMatching() throws InterruptedException {
     AVar<Integer> res1 = new AVar<>(1);
