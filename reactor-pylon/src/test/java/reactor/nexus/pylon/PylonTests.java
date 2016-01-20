d6f263c696f3e000faf07342fc38d1c7e00a6183
Stephane Maldini <smaldini@pivotal.io>
2016-01-20 18:32:54 +0000

    move ReactiveSession

--- reactor-pylon/src/test/java/reactor/nexus/pylon/PylonTests.java
+++ reactor-pylon/src/test/java/reactor/nexus/pylon/PylonTests.java
@@ -19,7 +19,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
-import reactor.core.subscription.ReactiveSession;
+import reactor.core.subscriber.ReactiveSession;
 import reactor.core.timer.Timers;
 import reactor.core.util.Exceptions;
 import reactor.io.net.ReactiveNet;
